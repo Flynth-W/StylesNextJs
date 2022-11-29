ac21b5ae7bb1fecc48d4af45f396c5294ae373fa
flynth <flynth.dev@gmail.com>
2022-11-29 16:12:52 -0400

    Error localStore.getItem('theme')

--- src/components/Themes/button.tsx
+++ src/components/Themes/button.tsx
@@ -2,9 +2,10 @@ import { useTheme } from 'next-themes'
 
 export const ThemeChanger = () => {
   const { theme, setTheme } = useTheme()
+  // error ?
+  //<div> The current theme is: {theme} </div>
   return (
     <div>
-      <div> The current theme is: {theme} </div>
       <button onClick={() => setTheme('light')}>Light Mode</button>
       <button onClick={() => setTheme('dark')}>Dark Mode</button>
       <button onClick={() => setTheme('blue')}>Blue Mode</button>
