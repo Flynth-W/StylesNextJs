7d2e0e002261ad14260d7d7657e472263c3bbb1a
flynth <flynth.dev@gmail.com>
2022-11-29 18:23:03 -0400

    Include: Footer & Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,16 @@
 import { ThemeChanger } from "./Themes/button"
+import { Header } from "./Header/main"
+import { Footer } from "./Footer/main"
+
 export default function Layout(props:any){
   return (
   <div>
+    <Header/>
     <main>
         {props.children}
     </main>
     <ThemeChanger/>
+    <Footer/>
   </div>
       )
 }
