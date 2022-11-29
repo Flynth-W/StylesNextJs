3a67b286ebb8e6ea695c898ace46fee83a18da79
flynth <flynth.dev@gmail.com>
2022-11-29 17:48:44 -0400

    Fix: React Hydration Error

--- src/components/Themes/button.tsx
+++ src/components/Themes/button.tsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 
 export const ThemeChanger = () => {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
-  // error ?
-  //<div> The current theme is: {theme} </div>
+  
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+
   return (
     <div>
+      <div> The current theme is: {theme} </div>
       <button onClick={() => setTheme('light')}>Light Mode</button>
       <button onClick={() => setTheme('dark')}>Dark Mode</button>
       <button onClick={() => setTheme('blue')}>Blue Mode</button>
